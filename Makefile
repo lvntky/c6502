22bdaf749386f9283e1e900aee384275d4d87571
lvntky <klevent1903@gmail.com>
2024-08-24 12:55:11 +0300

    [initial] gui added

---
+++ Makefile
@@ -0,0 +1,29 @@
+CC = gcc
+CFLAGS = -Wall -std=c99 -Iinclude
+LIBS = -lraylib
+
+SRC_DIR = src
+BUILD_DIR = build
+TARGET = c6502
+
+# Automatically find all .c files in the SRC_DIR
+SRC = $(wildcard $(SRC_DIR)/*.c)
+
+# Create corresponding .o files in the BUILD_DIR
+OBJ = $(patsubst $(SRC_DIR)/%.c, $(BUILD_DIR)/%.o, $(SRC))
+
+all: $(TARGET)
+
+$(TARGET): $(OBJ)
+	$(CC) $(CFLAGS) -o $@ $^ $(LIBS)
+
+# Rule to compile .c files into .o files
+$(BUILD_DIR)/%.o: $(SRC_DIR)/%.c | $(BUILD_DIR)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+# Create the build directory if it doesn't exist
+$(BUILD_DIR):
+	mkdir -p $(BUILD_DIR)
+
+clean:
+	rm -rf $(BUILD_DIR) $(TARGET)
--- src/main.c
+++ src/main.c
@@ -1,57 +1,23 @@
-#include <stdio.h>
-#include "../include/cpu.h"
+#include <raylib.h>
 
-// DEBUG PURPOSE ONLY, DELETE IT.
-int main(int argc, char** argv)
-{
-  Cpu testCpu;
-  reset_cpu(&testCpu);
+int main(void) {
+    // Initialization
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
 
-  const unsigned char program[] = {
-      0x18,
-      0xD8,
-      0x58,
-      0xB8,
-      0xCA,
-      0x88,
-      0xE8,
-      0xC8,
-      0xEA,
-      0x48,
-      0x08,
-      0x28,
-      0x68,
-      0x38,
-      0xF8,
-      0x78,
-      0xAA,
-      0xA8,
-      0x8A,
-      0x98,
-      0x9A,
-      0x69, 0x10,
-      0x29, 0x10,
-      0xC9, 0x10,
-      0xE0, 0x10,
-      0xC0, 0x10,
-      0xA9, 0x10,
-      0xA2, 0x10,
-      0xA0, 0x10,
-      0x09, 0x10,
-      0x00
-  };
+    InitWindow(screenWidth, screenHeight, "C6502");
 
-  int program_size = sizeof(program) / sizeof(program[0]);
-  run(&testCpu, program, program_size);
-  printf("accumulator : %d\n", testCpu.register_a);
-  printf("x register : %d\n", testCpu.register_x);
-  printf("y register : %d\n", testCpu.register_y);
-  printf("pc %d\n", testCpu.program_counter);
-  printf("sp: %d\n", STACK_PAGE_START + testCpu.stack_pointer);
-  for (int i = 510; i < 512; i++) {
-    printf("STACK (%d) - %d\n", (i), testCpu.memory[i]);
-  }
+    // Main emulation loop
+    while (!WindowShouldClose()) {
+        // Draw
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText("c6502 Emulator and Debugger", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
 
-  print_cpu_flags(&testCpu);
-  return 0;
+    // De-Initialization
+    CloseWindow();
+
+    return 0;
 }
